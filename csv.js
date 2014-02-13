083cb0f2bf5a982e4bc339d84e4383344e3535b0
Jorge O. Blanchard Cruz <rothgard@rothgard-VirtualBox.(none)>
2014-02-13 20:50:02 +0000

    commit code

--- csv.js
+++ csv.js
@@ -1,4 +1,4 @@
-// See http://en.wikipedia.org/wiki/Comma-separated_values
+
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 
 $(document).ready(function() {
@@ -6,21 +6,30 @@ $(document).ready(function() {
      calculate();
    });
  });
+ 
+ window.onload = function() {
+
+  if (window.localStorage && localStorage.str_input) {
+    document.getElementById("textbox_input").value = localStorage.str_input;
+  }
+};
 
 function calculate() {
   var result;
-  var original = document.getElementById("original");
-  var temp = original.value;
+  var str_input = document.getElementById("textbox_input");
+  var temp = str_input.value;
   var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;
   var lines = temp.split(/\n+\s*/);
   var commonLength = NaN;
   var r = [];
+  
   // Template using underscore
   var row = "<% _.each(items, function(name) { %>" +
             " <td><%= name %></td>" +
             " <% }); %>";
 
-  if (window.localStorage) localStorage.original = temp;
+  if (window.localStorage) 
+	localStorage.original = temp;
   
   for(var t in lines) {
     var temp = lines[t];
@@ -37,31 +46,30 @@ function calculate() {
         commonLength = m.length;
         error = false;
       }
+      
       for(var i in m) {
-        var removecomma = m[i].replace(/,\s*$/,'');
-        var remove1stquote = removecomma.replace(/^\s*"/,'');
-        var removelastquote = remove1stquote.replace(/"\s*$/,'');
-        var removeescapedquotes = removelastquote.replace(/\\"/,'"');
-        result.push(removeescapedquotes);
+        m[i] = m[i].replace(/,\s*$/,'');
+        m[i]= m[i].replace(/^\s*"/,'');
+        m[i] = m[i].replace(/"\s*$/,'');
+        m[i] = m[i].replace(/\\"/,'"');
+        
+        result.push(m[i]);
       }
+      
       var tr = error? '<tr class="error">' : '<tr>';
       r.push(tr+_.template(row, {items : result})+"</tr>");
     }
     else {
-      alert('ERROR! row '+temp+' does not look as legal CSV');
+      alert('ERROR! row ' + temp + ' does not look as legal CSV');
       error = true;
     }
   }
+  
   r.unshift('<p>\n<table class="center" id="result">');
   r.push('</table>');
-  //alert(r.join('\n')); // debug
-  finaltable.innerHTML = r.join('\n');
+  table_output.innerHTML = r.join('\n');
+  
 }
 
-window.onload = function() {
-  // If the browser supports localStorage and we have some stored data
-  if (window.localStorage && localStorage.original) {
-    document.getElementById("original").value = localStorage.original;
-  }
-};
+
 
